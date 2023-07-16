9aa02ae9446885da4a9b48ec3db67ddefa96e12d
Cuttlas <mohsenbarekati@gmail.com>
2023-07-16 20:44:28 +0330

    removing fronts

--- front/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import serviceReducer from '../features/service/serviceSlice';
-
-export const store = configureStore({
-  reducer: {
-    service: serviceReducer,
-  },
-});
